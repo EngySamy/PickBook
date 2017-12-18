26aee2904a96654810c2b2141f4bdfcaf3383346
EngySamy <engy.elshorbagy96@eng-st.cu.edu.eg>
2017-12-18 20:08:12 +0200

    fix adding review -- but not view .

--- database/migrations/2017_12_08_070638_create_reviews_table.php
+++ database/migrations/2017_12_08_070638_create_reviews_table.php
@@ -20,7 +20,6 @@ class CreateReviewsTable extends Migration
             $table->text('value');
             $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('item_id')->references('id')->on('items')->onUpdate('cascade')->onDelete('cascade');
-            $table->primary(['user_id', 'item_id']);
             $table->timestamps();
         });
     }
